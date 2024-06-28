4206c12b5124ecbab5bd7cec8a9e49a34b903efd
JanBuegel <jan.buegel@gmail.com>
2024-06-28 13:08:18 +0200

    bash script update

--- db_import.sh
+++ db_import.sh
@@ -8,6 +8,7 @@ fi
 
 DOCKER_CONTAINER_ID=$1
 JSON_FILE="of_artists.json"
+CONTAINER_PATH="/data/$JSON_FILE"
 
 # Überprüfen, ob die JSON-Datei existiert
 if [ ! -f "$JSON_FILE" ]; then
@@ -15,7 +16,10 @@ if [ ! -f "$JSON_FILE" ]; then
   exit 1
 fi
 
+# JSON-Datei in den Docker-Container kopieren
+docker cp $JSON_FILE $DOCKER_CONTAINER_ID:$CONTAINER_PATH
+
 # JSON-Datei in die MongoDB in Docker importieren
-docker exec -i $DOCKER_CONTAINER_ID mongoimport --db open-flair --collection artists --drop --file /data/$JSON_FILE --jsonArray
+docker exec -i $DOCKER_CONTAINER_ID mongoimport --db open-flair --collection artists --drop --file $CONTAINER_PATH --jsonArray
 
 echo "Import completed."
